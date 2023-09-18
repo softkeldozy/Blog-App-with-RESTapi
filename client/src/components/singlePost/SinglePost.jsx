3952224fa9ab3ec40319c0b5784f9822bed8bbc2
Kelvin Obidozie <kelvinobidozie@programmer.net>
2023-09-18 22:26:02 +0100

    finished wite and single post page functionality

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -13,24 +13,36 @@ function SinglePost() {
   const path = location.pathname.split('/')[2];
   const [post, setPost] = useState([]);
   const { user } = useContext(Context);
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+  const [updateMode, setUpdateMode] = useState(false)
   // Whenever this path changes fire this useEffect
   useEffect(() => {
     const fetchPost = async () => {
       const res = await axios.get('/posts/' + path);
       setPost(res.data);
+      setTitle(res.data.title);
+      setDesc(res.data.desc);
     }
     fetchPost();
   }, [path])
 
   const handleDelete = async () => {
-    // e.preventDefault();
     try {
-      await axios.delete("/posts/" + path, { data: { username: user.username } });
+      await axios.delete(`/posts/ ${post._id}`, { data: { username: user.username } });
       window.location.replace('/');
     } catch (err) {
 
     }
   };
+  const handleUpdate = async () => {
+    try {
+      await axios.put(`/posts/${post._id}`, { username: user.username, title, desc });
+      setUpdateMode(false)
+    } catch (err) {
+
+    }
+  }
   return (
     <div className='singlePost'>
       <div className="singlePostWrapper">
@@ -39,15 +51,17 @@ function SinglePost() {
           className='singlePostImg'
           src={PF + post.photo}
           alt='' />
+        )}{updateMode ? <input type='text' value={title} className='singlePostTitleInput' autoFocus onChange={(e) => setTitle(e.target.value)} /> : (
+          <h1 className="singlePostTitle">{title}
+            {post.username === user?.username &&
+              <div className="singlePostEdit">
+                <i className="singlePostIcon far fa-edit" onClick={() => setUpdateMode(true)}></i>
+                <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
+              </div>
+            }
+          </h1>
         )}
-        <h1 className="singlePostTitle">{post.title}
-          {post.username === user?.username &&
-            <div className="singlePostEdit">
-              <i className="singlePostIcon far fa-edit"></i>
-              <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
-            </div>
-          }
-        </h1>
+
         <div className="singlePostInfo">
           {/* Redirecting query and populating category search according to username */}
           <span className='singlePostAuthor'>
@@ -58,7 +72,8 @@ function SinglePost() {
           </span>
           <span className='singlePostDate'>{new Date(post.createdAt).toDateString()}</span>
         </div>
-        <p className='singlePostDesc'>{post.desc}</p>
+        {updateMode ? (<textarea className='singlePostDescInput' value={desc} onChange={(e) => setDesc(e.target.value)} />) : (<p className='singlePostDesc'>{desc}</p>)}
+        {updateMode && <button className="singlePostButton" onClick={handleUpdate}>Update</button>}
       </div>
     </div >
   )
