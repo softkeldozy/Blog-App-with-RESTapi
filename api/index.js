2f483d20d70ba4e1d9b1abee4a0090cb422b960d
Kelvin Obidozie <kelvinobidozie@programmer.net>
2023-09-18 09:30:52 +0100

    Post and delete article functionality created

--- api/index.js
+++ api/index.js
@@ -6,12 +6,15 @@ const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
 const multer = require('multer');
+const path = require('path');
 
 
 const app = express();
 dotenv.config();
 // Enabling the app to send onjects in json format
 app.use(express.json());
+// for posting of images during article creation
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 /******************
 Database Connection
--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,13 +1,18 @@
 import { Link, useLocation } from 'react-router-dom'
 import './singlePost.css'
 import axios from 'axios'
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
+import { Context } from '../../context/context';
+
 
 function SinglePost() {
+  const PF = 'http://localhost:5000/images/'
+
   // Getting the user for a post written
   const location = useLocation();
   const path = location.pathname.split('/')[2];
   const [post, setPost] = useState([]);
+  const { user } = useContext(Context);
   // Whenever this path changes fire this useEffect
   useEffect(() => {
     const fetchPost = async () => {
@@ -17,21 +22,31 @@ function SinglePost() {
     fetchPost();
   }, [path])
 
+  const handleDelete = async () => {
+    // e.preventDefault();
+    try {
+      await axios.delete("/posts/" + path, { data: { username: user.username } });
+      window.location.replace('/');
+    } catch (err) {
 
+    }
+  };
   return (
     <div className='singlePost'>
       <div className="singlePostWrapper">
         {/* if any image exists */}
         {post.photo && (<img
           className='singlePostImg'
-          src={post.photo}
+          src={PF + post.photo}
           alt='' />
         )}
         <h1 className="singlePostTitle">{post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon far fa-trash-alt"></i>
-          </div>
+          {post.username === user?.username &&
+            <div className="singlePostEdit">
+              <i className="singlePostIcon far fa-edit"></i>
+              <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
+            </div>
+          }
         </h1>
         <div className="singlePostInfo">
           {/* Redirecting query and populating category search according to username */}
